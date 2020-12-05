4e0e5b7ff36df70fa9ceed00a8a1c5f069104a14
rlopez-lab <74743209+rlopez-lab@users.noreply.github.com>
2020-12-05 14:53:24 -0600

    construccion de clases

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -1,4 +1,33 @@
 
 public class Persona {
+    private int idPersona;
+    private String nombre;
+    private static int contadorPersonas;
     
+    public Persona (String nombre){
+        this.idPersona = ++contadorPersonas;
+        this.nombre = nombre;
+    }
+    
+    public int getIdPersona(){
+        return this.idPersona;
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public static int getContadorPersonas(){
+        return contadorPersonas;
+    }
+    
+    @Override
+    public String toString(){
+        return "idPersona: " + idPersona + ", nombre: " + nombre + 
+                ", contadorPersonas: " + contadorPersonas;
+    }
 }
